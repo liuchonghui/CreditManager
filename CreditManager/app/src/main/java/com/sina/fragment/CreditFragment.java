e16a30dc0bc0bbee23749eca29d41cce1da7903e
liu_chonghui <liuchonghui@139.com>
2016-04-02 18:30:24 +0800

    commit changes

---
+++ CreditManager/app/src/main/java/com/sina/fragment/CreditFragment.java
@@ -0,0 +1,101 @@
+package com.sina.fragment;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sina.sinagame.credit.R;
+
+/**
+ * @author liu_chonghui
+ */
+public class CreditFragment extends BaseFragment {
+
+    protected int getPageLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startTransaction();
+        initData();
+    }
+
+    protected void startTransaction() {
+        getActivity().overridePendingTransition(R.anim.push_left_in,
+                R.anim.push_still);
+    }
+
+    protected void initData() {
+        if (getActivity().getIntent() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        if (!isViewNull()) {
+            return mView;
+        }
+        mView = inflater.inflate(getPageLayout(), container, false);
+        intView(mView);
+        return mView;
+    }
+
+    @SuppressLint("InflateParams")
+    protected void intView(View view) {
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean holdGoBack() {
+        // if (myOneKeyShare != null && myOneKeyShare.isShow()) {
+        // return true;
+        // }
+        // if (popupAttacher != null && popupAttacher.isShowing()) {
+        // return true;
+        // }
+        return false;
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        boolean flag = false;
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (holdGoBack()) {
+                // if (myOneKeyShare != null && myOneKeyShare.isShow()) {
+                // myOneKeyShare.close();
+                // }
+                // if (popupAttacher != null && popupAttacher.isShowing()) {
+                // popupAttacher.closePop();
+                // }
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    public void leaveCurrentPage() {
+        getActivity().finish();
+        getActivity().overridePendingTransition(R.anim.push_still,
+                R.anim.push_right_out);
+    }
+}
